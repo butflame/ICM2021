64633482ee82ba4fe84222fb36dc96ded1044e8a
wangbinhan <wangbinhan@xiaoduotech.com>
2021-02-08 00:28:16 +0800

    finish kmeans

--- process/step2.py
+++ process/step2.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import os
 
@@ -31,34 +32,73 @@ class Artist(dict):
 
 
 def run():
-    artist_datas = [Artist(d) for d in data_by_artist]
+    genre_by_artist = {}
+    genres = set()
+    artist_datas = []
+    for d in data_by_artist:
+        genre_by_artist[int(d["artist_id"])] = d["main_genre"]
+        genres.add(d["main_genre"])
+        artist_datas.append(Artist(d))
+
     focus_points, groups = kmeans(
         artist_datas,
+        k=20,  # genre数量, 除开Unknown共19个
+        eva_diff=Artist.diff,
+        eva_avg=Artist.avg,
+        threshold=0.01,
+    )
+    with open(os.path.join(BASE_DIR, 'process_data', 'step2_by_artist_focus_point.csv'), 'w') as f:
+        writer = csv.DictWriter(f, music_feature_keys)
+        writer.writeheader()
+        writer.writerows(focus_points)
+
+    genre_count_by_group = []
+    for index, grouped_datas in enumerate(groups, start=1):
+        tmp = {"group": str(index)}
+        for d in grouped_datas:
+            cur_genre = genre_by_artist.get(int(d['artist_id']), 'Unknown')
+            tmp[cur_genre] = tmp[cur_genre] + 1 if cur_genre in tmp else 1
+            for g in genres:
+                tmp.setdefault(g, 0)
+        genre_count_by_group.append(tmp)
+
+    with open(os.path.join(BASE_DIR, 'process_data', 'step2_by_artist_genre_count_by_group.csv'), 'w') as f:
+        # 每个聚类组中各个流派的分布
+        header = ["group", *sorted(genres)]
+        writer = csv.DictWriter(f, header)
+        writer.writeheader()
+        writer.writerows(genre_count_by_group)
+
+    # full_music_datas
+    music_datas = [Artist(d) for d in full_music_data]
+    focus_points, groups = kmeans(
+        music_datas,
         k=20,  # genre数量
         eva_diff=Artist.diff,
         eva_avg=Artist.avg,
-        threshold=0.05,  # fixme: 可以自己调整, 大一点可以跑快点，越小可以越精确
+        threshold=0.01,
     )
-    with open(os.path.join(BASE_DIR, 'process_data', 'step2_artist_kmeans.json'), 'w') as f:
-        f.write(json.dumps({
-            "focus_points": focus_points,
-            "groups": groups,
-        }))
-    #
-    # # full_music_datas
-    # music_datas = [Artist(d) for d in full_music_data]
-    # focus_points, groups = kmeans(
-    #     music_datas,
-    #     k=20,  # genre数量
-    #     eva_diff=Artist.diff,
-    #     eva_avg=Artist.avg,
-    #     threshold=0.05,  # fixme: 可以自己调整
-    # )
-    # with open(os.path.join(BASE_DIR, 'process_data', 'step2_music_kmeans.json'), 'w') as f:
-    #     f.write(json.dumps({
-    #         "focus_points": focus_points,
-    #         "groups": groups,
-    #     }))
+    with open(os.path.join(BASE_DIR, 'process_data', 'step2_by_music_focus_point.csv'), 'w') as f:
+        writer = csv.DictWriter(f, music_feature_keys)
+        writer.writeheader()
+        writer.writerows(focus_points)
+
+    genre_count_by_group = []
+    for index, grouped_datas in enumerate(groups, start=1):
+        tmp = {"group": str(index)}
+        for d in grouped_datas:
+            for artist_id in json.loads(d["artists_id"]):
+                cur_genre = genre_by_artist[artist_id]
+                tmp[cur_genre] = tmp[cur_genre] + 1 if cur_genre in tmp else 1
+            for g in genres:
+                tmp.setdefault(g, 0)
+        genre_count_by_group.append(tmp)
+    with open(os.path.join(BASE_DIR, 'process_data', 'step2_by_music_genre_count_by_group.csv'), 'w') as f:
+        # 每个聚类组中各个流派的分布
+        header = ["group", *sorted(genres)]
+        writer = csv.DictWriter(f, header)
+        writer.writeheader()
+        writer.writerows(genre_count_by_group)
 
 
 if __name__ == "__main__":
